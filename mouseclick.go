b98ea0a5079c790d14cc54fc8cb1aea0ecea1341
grchar <806762029@qq.com>
2022-03-10 11:09:38 +0800

    连点器实现

---
+++ mouseclick.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/go-vgo/robotgo"
+)
+
+func main() {
+	for {
+		fmt.Println("请按s开始连点,按e结束连点")
+		var sw sync.WaitGroup
+		ch := make(chan bool, 8)
+		start := robotgo.AddEvent("s")
+
+		go func() {
+			exit := robotgo.AddEvent("e")
+			if exit {
+				for i := 0; i < 8; i++ {
+					ch <- true
+				}
+			}
+
+		}()
+
+		count := 0
+		if start {
+
+			// go func() {
+			// 	time.Sleep(1 * time.Second)
+			// 	for i := 0; i < 8; i++ {
+			// 		ch <- true
+			// 	}
+			// }()
+			fmt.Println("开始连点")
+			for i := 0; i < 8; i++ {
+				sw.Add(1)
+				go func() {
+					for {
+						select {
+						case <-ch:
+							sw.Done()
+							return
+						default:
+							// x, y := robotgo.GetMousePos()
+							robotgo.Click(`left`, false)
+							count++
+							time.Sleep(1 * time.Microsecond)
+						}
+					}
+				}()
+			}
+
+		}
+		sw.Wait()
+		fmt.Println("停止连点", count)
+	}
+}

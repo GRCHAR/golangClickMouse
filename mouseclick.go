5fc53e74fb3e89f560d46ee4639cfe04b7942651
grchar <806762029@qq.com>
2022-03-10 11:51:00 +0800

    添加多线程点击计数

--- mouseclick.go
+++ mouseclick.go
@@ -13,10 +13,12 @@ func main() {
 		fmt.Println("请按s开始连点,按e结束连点")
 		var sw sync.WaitGroup
 		ch := make(chan bool, 8)
+		resultCh := make(chan int, 8)
 		start := robotgo.AddEvent("s")
 
 		go func() {
 			exit := robotgo.AddEvent("e")
+
 			if exit {
 				for i := 0; i < 8; i++ {
 					ch <- true
@@ -24,35 +26,41 @@ func main() {
 			}
 
 		}()
-
 		if start {
-
-			// go func() {
-			// 	time.Sleep(1 * time.Second)
-			// 	for i := 0; i < 8; i++ {
-			// 		ch <- true
-			// 	}
-			// }()
 			fmt.Println("开始连点")
 			for i := 0; i < 8; i++ {
 				sw.Add(1)
+
 				go func() {
+					count := 0
 					for {
 						select {
 						case <-ch:
+							resultCh <- count
 							sw.Done()
 							return
 						default:
 							// x, y := robotgo.GetMousePos()
 							robotgo.Click(`left`, false)
+							count++
 							time.Sleep(1 * time.Millisecond)
 						}
 					}
 				}()
+
 			}
 
 		}
+
 		sw.Wait()
-		fmt.Println("停止连点")
+		totalCount := 0
+
+		for i := 0; i < 8; i++ {
+			totalCount += <-resultCh
+		}
+
+		close(resultCh)
+		close(ch)
+		fmt.Println("停止连点", totalCount)
 	}
 }
